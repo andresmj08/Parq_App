4d36c03a64ec6d03b36c5709a44b844a85d655d1
andresmj08 <andresmj08@gmail.com>
2021-06-04 00:50:44 -0500

    Update Info_parq_route

--- src/pages/Info_Parq_Route.js
+++ src/pages/Info_Parq_Route.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Alert, Modal, StyleSheet, Text, Pressable, View } from "react-native";
+import { Alert, Modal, StyleSheet, Text, Pressable, View, Image } from "react-native";
 import { Icon } from 'react-native-elements';
 
 
@@ -8,6 +8,12 @@ const Info_Parq_Route = (props) => {
   const [modalVisible, setModalVisible] = useState(false);
   const id_parqueadero = props.Id_Parq;
   const [register_parq, setRegister] = useState(false);
+  const [Datos_Parq, setDatos_Parq] = useState({
+    hora_moto: 300,
+    hora_carro: 2600,
+    apertura: '08:00',
+    cierre: '18:00'
+  });
   
 
   fetch('http://34.217.178.10/Conexion_Parq_app/info_sitios.php', {
@@ -46,12 +52,43 @@ const Info_Parq_Route = (props) => {
       >
         <View style={styles.centeredView}>
           <View style={styles.modalView}> 
+
+          <View style={styles.row}>
+            <View style={[styles.fraccion, styles.unique_box, styles.button]}>
+              <Text style={styles.bold_text}>Valor de Fracción</Text>
+            </View>
+          </View>
+
+          <View style={styles.row}>
+            <View style={[styles.fraccion]}>
+              <Image source={require("../components/imagenes/Carro.png")} style={{ width: '50%', height: '50%', resizeMode: 'contain' }} />
+            </View>
+
+            <View style={[styles.fraccion]}>
+              <Image source={require("../components/imagenes/Moto.png")} style={{ width: '50%', height: '50%', resizeMode: 'contain' }} />
+            </View>
+          </View>
+
+          
+          <View style={styles.row}>
+            <View style={[styles.fraccion,  styles.valor_box]}>
+              <Text style={styles.bold_text}>{"\n"}$ {Datos_Parq.hora_carro}</Text>
+            </View>
+            <View style={[styles.fraccion,  styles.valor_box]}>
+              <Text style={styles.bold_text}>{"\n"}$ {Datos_Parq.hora_moto}</Text>
+            </View>
+          </View>
+          
+
+
+
+          <Text style={styles.modalText}>{"\n"}<Icon name='calendar' size={30} type='evilicon'  color='#b6ad05'/>Horario de Atención de:{"\n"} <Text style={styles.bold_text}>{Datos_Parq.apertura}</Text> hasta: <Text style={styles.bold_text}>{Datos_Parq.cierre}</Text></Text>
            {register_parq 
-              ? <Text style={styles.register_ok}> <Icon name='check' size={30} type='evilicon'  color='green'/> Parqueadero Registrado en CC</Text>
-              : <Text style={styles.no_register}> <Icon name='exclamation' size={30} type='evilicon'  color='red'/> Parqueadero sin registro en CC</Text>
+              ? <Text style={styles.register_ok}> <Icon name='check' size={30} type='evilicon'  color='green'/>Registrado en Cámara de Comercio{"\n"}</Text>
+              : <Text style={styles.no_register}> <Icon name='exclamation' size={30} type='evilicon'  color='red'/> Sin registro en Cámara de Comercio{"\n"}</Text>
             }
             <Text style={styles.modalText}><Icon name='location' size={30} type='evilicon'  color='#b6ad05'/>Estas a {props.distancia} Km hasta <Text style={styles.bold_text}>{props.Parqueadero}</Text></Text>
-            <Text style={styles.modalText}><Icon name='clock' size={30} type='evilicon'  color='#b6ad05'/>Con un promedio {props.tiempo} Minutos</Text>
+            <Text style={styles.modalText}><Icon name='clock' size={30} type='evilicon'  color='#b6ad05'/>Con un promedio de {props.tiempo} Minutos</Text>
             <Pressable
               style={[styles.button, styles.buttonClose]}
               onPress={() => setModalVisible(!modalVisible)}
@@ -76,7 +113,9 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    marginTop: 400
+    marginTop: 400,
+    
+    
   },
   modalView: {
     margin: 20,
@@ -123,6 +162,25 @@ const styles = StyleSheet.create({
   },
   bold_text: {
     fontWeight: "bold"
+  },
+  row: {
+    flex: 1,
+    flexDirection: 'row',
+    
+  },
+  fraccion: {
+    flex: 1,
+    height: 100,
+    alignItems: "center",
+  },
+  
+  valor_box: {
+    height: 30
+  },
+  unique_box: {
+    flex:2,
+    backgroundColor: "#ffd600",
+    height: 30
   }
 });
 
